16f2cc0d57dd2d5c4d4921dfe7dc2a2041e250a0
vuyisa01 <218192215@mycput.ac.za>
2021-05-16 02:42:59 +0200

    First Commit

---
+++ src/main/java/za/ac/cput/Employee.java
@@ -0,0 +1,54 @@
+package za.ac.cput;
+
+public class Employee {
+    private String firstName, lastName;
+    private int employeeID;
+    private Double Salary;
+
+    public Employee(String firstName) {
+        this.firstName = firstName;
+    }
+    public Employee(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public Double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(Double salary) {
+        Salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", employeeID=" + employeeID +
+                '}';
+    }
+}
